faef03a8cb3ed1f694124f20de18d87fcc7a7cbb
plinio.salazar@yahoo.com.br <plinio.salazar@yahoo.com.br>
2023-06-09 17:39:10 -0300

    feat: add Formulario component

--- study-tracker/src/App.vue
+++ study-tracker/src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <main class="columns is-gapless is multiline">
     <div class="column is-one-quarter">
-        <BarraLateral/>
+      <BarraLateral/>
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <FormularioComponent />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue'
+import FormularioComponent from './components/Fomulario.vue'
 
 
 export default defineComponent({
   name: 'App',
   components: {
-    BarraLateral
+    BarraLateral,
+    FormularioComponent,
   }
 });
 </script>
